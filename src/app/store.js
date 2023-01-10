01f59c0ff0cde275ab6b37d2eee06f4393745fa9
SadrilAmin1999 <sadrilshuvo420@gmail.com>
2023-01-10 20:40:31 +0600

    clear cart done

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import productsReducer, {
   productsFetching,
 } from "../features/products/productSlice";
+import cartReducer from "../features/products/cartSlice";
 
 export const store = configureStore({
   reducer: {
     // multifle reducer goes here......
     products: productsReducer,
+    cart: cartReducer,
   },
 });
 store.dispatch(productsFetching());
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,46 +1,13 @@
-import { useState } from "react";
 import { currencyFormatter } from "../utlities/currencyFormatter";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, removeFromCart } from "../features/products/cartSlice";
 
-const data = [
-  {
-    id: 1,
-    name: "Blink Mini – Compact indoor plug-in smart security camera",
-    description:
-      "Monitor the inside of your home day and night with our 1080P HD indoor plug-in smart security camera",
-    price: 64.99,
-    image:
-      "https://res.cloudinary.com/dy28teazb/image/upload/v1668172648/React%20Shopping/Products/81-585-013-01_a04wkd.jpg",
-    category: "Camera",
-  },
-  {
-    id: 2,
-    name: "Vlogging Camera, 4K Digital Camera for YouTube with WiFi",
-    description:
-      "It's super suitable for the 'happy snapper' who just hope to point and shoot to take good quality images",
-    price: 109.99,
-    image:
-      "https://res.cloudinary.com/dy28teazb/image/upload/v1668172649/React%20Shopping/Products/81pgsjFGpmL_qundpd.jpg",
-    category: "Camera",
-  },
-  {
-    id: 3,
-    name: "SAMSUNG 55-Inch Class Crystal 4K UHD AU8000 Series HDR",
-    description:
-      "Witness millions of shades of color through powerful Dynamic Crystal technology",
-    price: 497.99,
-    image:
-      "https://res.cloudinary.com/dy28teazb/image/upload/v1668172649/React%20Shopping/Products/cl-uhd-tu7090-un50tu7090gxzs-rperspective-285965740_duusj5.png",
-    category: "TV",
-  },
-];
 const Cart = () => {
-  const [count, setCount] = useState(1);
-  const handleIncrease = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
-  const handleDecrease = () => {
-    setCount((prevCount) => prevCount - 1);
+  const { cartItems: data } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const handleRemove = (product) => {
+    dispatch(removeFromCart(product));
   };
   return (
     <div className="cart-section container mx-auto py-10">
@@ -65,7 +32,10 @@ const Cart = () => {
                 />
                 <div className="details flex flex-col gap-3 items-start">
                   <span>{product.name}</span>
-                  <button className="uppercase text-gray-400 hover:text-rose-500 duration-300">
+                  <button
+                    onClick={() => handleRemove(product)}
+                    className="uppercase text-gray-400 hover:text-rose-500 duration-300"
+                  >
                     Remove
                   </button>
                 </div>
@@ -74,19 +44,13 @@ const Cart = () => {
                 {currencyFormatter(product.price)}
               </div>
               <div className="counter flex">
-                <button
-                  onClick={() => handleDecrease()}
-                  className="h-10 w-10 bg-gray-100  border border-gray-300 active:bg-gray-700 active:text-gray-50"
-                >
+                <button className="h-10 w-10 bg-gray-100  border border-gray-300 active:bg-gray-700 active:text-gray-50">
                   -
                 </button>
                 <span className="h-10 w-10 bg-gray-100 flex justify-center items-center border border-gray-300">
-                  {count}
+                  10
                 </span>
-                <button
-                  onClick={() => handleIncrease()}
-                  className="h-10 w-10 bg-gray-100 border border-gray-300 active:bg-gray-700 active:text-gray-50"
-                >
+                <button className="h-10 w-10 bg-gray-100 border border-gray-300 active:bg-gray-700 active:text-gray-50">
                   +
                 </button>
               </div>
@@ -98,7 +62,10 @@ const Cart = () => {
         </div>
       </div>
       <div className="cart-lower flex justify-between items-start py-10">
-        <button className="clear-btn uppercase border py-3 px-8 hover:bg-rose-200 hover:text-rose-600 font-medium hover:border-rose-200 duration-300">
+        <button
+          onClick={() => dispatch(clearCart())}
+          className="clear-btn uppercase border py-3 px-8 hover:bg-rose-200 hover:text-rose-600 font-medium hover:border-rose-200 duration-300"
+        >
           Clear cart
         </button>
         <div className="flex flex-col items-start gap-2">
