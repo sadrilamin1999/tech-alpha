c010ba69009d56f2cf45d49396e286c8f9b7a55d
Sadril Amin <sadrilshuvo420@gmail.com>
2023-03-25 21:35:45 +0600

    card customized

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,8 +13,8 @@ const Card = ({ product }) => {
   };
   return (
     <div className="product flex flex-col gap-2 bg-white shadow-md rounded-xl overflow-hidden hover:shadow-2xl">
-      <div className="img">
-        <img src={product.image} alt="{product.name}" />
+      <div className="img flex justify-center">
+        <img src={product.image} alt="{product.name}" className="h-[12rem]" />
       </div>
       <div className="texts  flex flex-col gap-2 px-5 pb-5">
         <span className="catagory-tag uppercase text-xs font-semibold tracking-widest text-orange-500">
@@ -28,7 +28,7 @@ const Card = ({ product }) => {
           </span>
           <button
             onClick={() => addToCartHandler(product)}
-            className=" uppercase bg-violet-500 text-violet-50 font-medium py-3 px-10 rounded-md hover:bg-orange-500 hover:text-orange-50 duration-300 shadow-lg shadow-violet-300 hover:shadow-orange-300"
+            className=" uppercase bg-violet-500 text-violet-50 font-medium py-3 px-10 rounded-md hover:bg-orange-500 hover:text-orange-50 duration-300 shadow-sm shadow-violet-300 hover:shadow-orange-300"
           >
             Add to cart
           </button>
